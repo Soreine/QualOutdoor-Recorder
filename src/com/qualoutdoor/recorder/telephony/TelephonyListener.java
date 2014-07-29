59050016fe80f33839d0aea3dba72a7b844ed418
Nicolas Gaborit <gaborit.nico@gmail.com>
2014-07-29 15:50:23 +0200

    Added Listener support for TelephonyService

--- src/com/qualoutdoor/recorder/telephony/TelephonyListener.java
+++ src/com/qualoutdoor/recorder/telephony/TelephonyListener.java
@@ -10,50 +10,47 @@ import java.util.List;
  */
 public class TelephonyListener {
 
-	/** Listen for changes to the device call state. */
-	public static final int LISTEN_CALL_STATE = 8;
-	/** Listen for changes to observed cell info. */
-	public static final int LISTEN_CELL_INFO = 1024;
-	/** Listen for changes to the data connection state (cellular). */
-	public static final int LISTEN_DATA_CONNECTION_STATE = 64;
-	/** Listen for changes to the device location */
-	public static final int LISTEN_LOCATION = 2048;
-	/** Listen for changes to the network type */
-	public static final int LISTEN_NETWORK_TYPE = 4096;
-	/** Stop listening for updates. */
-	public static final int LISTEN_NONE = 0;
-	/** Listen for changes to the network signal strengths (cellular). */
-	public static final int LISTEN_SIGNAL_STRENGTHS = 256;
+    /*********************************************
+     * Theses constant are used as bitwise mask, hence the power of two.
+     *********************************************/
 
-	/** Constructor */
-	public TelephonyListener() {
-	}
+    /** Listen for changes to the device call state. */
+    public static final int LISTEN_CALL_STATE = 8;
+    /** Listen for changes to observed cell info. */
+    public static final int LISTEN_CELL_INFO = 1024;
+    /** Listen for changes to the data connection state (cellular). */
+    public static final int LISTEN_DATA_CONNECTION_STATE = 64; 
+    /** Listen for changes to the device location */
+    public static final int LISTEN_LOCATION = 2048;
+    /** Listen for changes to the network type */
+    public static final int LISTEN_NETWORK_TYPE = 4096; 
+    /** Stop listening for updates. */
+    public static final int LISTEN_NONE = 0;
+    /** Listen for changes to the network signal strengths (cellular). */
+    public static final int LISTEN_SIGNAL_STRENGTHS = 256;
 
-	/** Callback invoked when device call state changes. */
-	public void onCallStateChanged(int state, String incomingNumber) {
-	}
+    /** Constructor */
+    public TelephonyListener() {}
 
-	/**
-	 * Callback invoked when a observed cell info has changed, or new cells have
-	 * been added or removed.
-	 */
-	public void onCellInfoChanged(List<ICellInfo> cellInfo) {
-	}
+    /** Callback invoked when device call state changes. */
+    public void onCallStateChanged(int state, String incomingNumber) {}
 
-	/** Callback invoked when device location changes. */
-	public void onLocationChanged(ILocation location) {
-	}
+    /**
+     * Callback invoked when a observed cell info has changed, or new cells have
+     * been added or removed.
+     */
+    public void onCellInfoChanged(List<ICellInfo> cellInfo) {}
 
-	/** Callback invoked when connection state changes. */
-	public void onDataConnectionStateChanged(int state) {
-	}
+    /** Callback invoked when device location changes. */
+    public void onLocationChanged(ILocation location) {}
 
-	/** Callback invoked when network type changes. */
-	public void onNetworkTypeChanged(int state, int networkType) {
-	}
+    /** Callback invoked when connection state changes. */
+    public void onDataConnectionStateChanged(int state) {}
 
-	/** Callback invoked when network signal strengths changes. */
-	public void onSignalStrengthsChanged(ISignalStrength signalStrength) {
-	}
+    /** Callback invoked when network type changes. */
+    public void onNetworkTypeChanged(int state, int networkType) {}
+
+    /** Callback invoked when network signal strengths changes. */
+    public void onSignalStrengthsChanged(ISignalStrength signalStrength) {}
 
 }
--- src/com/qualoutdoor/recorder/telephony/TelephonyService.java
+++ src/com/qualoutdoor/recorder/telephony/TelephonyService.java
@@ -7,9 +7,10 @@ import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.telephony.CellInfo;
 import android.telephony.PhoneStateListener;
-import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import com.qualoutdoor.recorder.telephony.TelephonyListener;
 import android.util.Log;
 
 /**
@@ -19,153 +20,224 @@ import android.util.Log;
  */
 public class TelephonyService extends Service implements ITelephony {
 
-	/** The interface binder for this service */
-	private IBinder mTelephonyBinder;
+    /** The interface binder for this service */
+    private IBinder mTelephonyBinder;
 
-	/** An instance of TelephonyManager */
-	private TelephonyManager telephonyManager;
+    /** An instance of TelephonyManager */
+    private TelephonyManager telephonyManager;
 
-	/** The events the phone state listener is monitoring */
-	@SuppressLint("InlinedApi")
-	private static int events = PhoneStateListener.LISTEN_SIGNAL_STRENGTHS
-			| PhoneStateListener.LISTEN_CALL_STATE
-			| PhoneStateListener.LISTEN_DATA_CONNECTION_STATE
-			| PhoneStateListener.LISTEN_SERVICE_STATE
-			| PhoneStateListener.LISTEN_CELL_INFO; // TODO support for earlier
-													// version (see
-													// NeighboringCellInfo)
+    /** The events the phone state listener is monitoring */
+    @SuppressLint("InlinedApi")
+    private static int events = PhoneStateListener.LISTEN_CALL_STATE
+            // | PhoneStateListener.LISTEN_SIGNAL_STRENGTHS we use the cells
+            // infos instead
+            | PhoneStateListener.LISTEN_DATA_CONNECTION_STATE
+            | PhoneStateListener.LISTEN_SERVICE_STATE
+            | PhoneStateListener.LISTEN_CELL_INFO;
 
-	/** The Android phone state listener */
-	private PhoneStateListener phoneStateListener = new PhoneStateListener() {
-		@Override
-		public void onSignalStrengthsChanged(SignalStrength ss) {
-			// Update our signal strength instance
-			TelephonyService.this.signalStrength.setSignalStrength(ss);
-		}
+    /** The Android phone state listener */
+    private PhoneStateListener phoneStateListener = new PhoneStateListener() {
 
-		@Override
-		public void onDataConnectionStateChanged(int state, int networkType) {
-			// Update the current data connection state
-			TelephonyService.this.dataState = state;
-			// Update the current network type
-			TelephonyService.this.networkType = networkType;
-		};
+        @Override
+        public void onDataConnectionStateChanged(int state, int networkType) {
+            // Update the current data connection state
+            TelephonyService.this.dataState = state;
+            // Update the current network type
+            TelephonyService.this.networkType = networkType;
+        };
 
-		@Override
-		public void onCallStateChanged(int state, String incomingNumber) {
-			// Update the current call state
-			TelephonyService.this.callState = state;
-		};
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            // Update the current call state
+            TelephonyService.this.callState = state;
+        };
 
-		@Override
-		public void onCellInfoChanged(List<android.telephony.CellInfo> cellInfo) {
-		};
-	};
+        @Override
+        public void onCellInfoChanged(List<CellInfo> cellInfos) {
+            // Reset the cell info array list
+            allCellInfos.clear();
 
-	/** The current signal strength value */
-	private CustomSignalStrength signalStrength;
-	/** The current data connection state */
-	private int dataState;
-	/** The current network type */
-	private int networkType;
-	/** The current call state */
-	private int callState;
+            // Declare the variable holding the converted cell
+            CustomCellInfo customCell;
+            // Update the current ICellInfo list and retrieve the signal
+            // strength at the same time.
+            for (CellInfo cell : cellInfos) {
+                // Create a corresponding ICellInfo and
+                customCell = CustomCellInfo.buildFromCellInfo(cell);
+                // Add it to the list
+                allCellInfos.add(customCell);
+                // Check if this is the primary cell
+                if (cell.isRegistered()) {
+                    // This is the signal strength you are looking for
+                    signalStrength = customCell.getSignalStrength();
+                }
+            }
 
-	/** TODO The current cell infos */
+        };
+    };
 
-	@Override
-	public void onCreate() {
-		// Initialize a TelephonyBinder that knows this Service
-		mTelephonyBinder = new TelephonyBinder(this);
+    /** The current signal strength value */
+    private ISignalStrength signalStrength;
+    /** The current data connection state */
+    private int dataState;
+    /** The current network type */
+    private int networkType;
+    /** The current call state */
+    private int callState;
+    
+    /** The current visible cells */
+    private ArrayList<ICellInfo> allCellInfos;
+    /** This is the estimated max size for the cell info array list */
+    private static final int ESTIMATED_MAX_CELLS = 10;
 
-		// Retrieve an instance of Telephony Manager
-		telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-		
-		// Initialize the current phone state values
-		{
-			// Initialize the signal strength
-			signalStrength = new CustomSignalStrength(null);
-			// Initialize the call state
-			callState = telephonyManager.getCallState();
-			// Initialize the network type
-			networkType = telephonyManager.getNetworkType();
-			// Initialize the data state
-			dataState = telephonyManager.getDataState();
-		}
-		
-		// Start listening to phone state
-		telephonyManager.listen(phoneStateListener, events);
+    /****** The listeners list ******/
+    /** Store the listeners listening to LISTEN_CALL_STATE */
+    private ArrayList<TelephonyListener> listenersCallState = new ArrayList<TelephonyListener>();
+    /** Store the listeners listening to LISTEN_CELL_INFO */
+    private ArrayList<TelephonyListener> listenersCellInfo = new ArrayList<TelephonyListener>();
+    /** Store the listeners listening to LISTEN_DATA_CONNECTION_STATE */
+    private ArrayList<TelephonyListener> listenersDataState = new ArrayList<TelephonyListener>();
+    /** Store the listeners listening to LISTEN_LOCATION */
+    private ArrayList<TelephonyListener> listenersLocation = new ArrayList<TelephonyListener>();
+    /** Store the listeners listening to LISTEN_NETWORK_TYPE */
+    private ArrayList<TelephonyListener> listenersNetworkType = new ArrayList<TelephonyListener>();
+    /** Store the listeners listening to LISTEN_SIGNAL_STRENGTHS */
+    private ArrayList<TelephonyListener> listenersSignalStrength = new ArrayList<TelephonyListener>();
 
-		super.onCreate();
-	}
+    @Override
+    public void onCreate() {
+        // Initialize a TelephonyBinder that knows this Service
+        mTelephonyBinder = new TelephonyBinder(this);
 
-	@Override
-	public void onDestroy() {
-		// Unregister our listener from the telephony manager system service
-		telephonyManager.listen(phoneStateListener,
-				PhoneStateListener.LISTEN_NONE);
-		super.onDestroy();
-	}
+        // Retrieve an instance of Telephony Manager
+        telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		Log.d("TelephonyService", intent.toString());
-		// Return our interface binder
-		return mTelephonyBinder;
-	}
+        // Initialize the current phone state values
+        {
+            // Initialize the signal strength
+            signalStrength = new CustomSignalStrength();
+            // Initialize the call state
+            callState = telephonyManager.getCallState();
+            // Initialize the network type
+            networkType = telephonyManager.getNetworkType();
+            // Initialize the data state
+            dataState = telephonyManager.getDataState();
+            // Initialize the cell list
+            allCellInfos = new ArrayList<ICellInfo>(ESTIMATED_MAX_CELLS);
+        }
 
-	@Override
-	public List<ICellInfo> getAllCellInfo() {
-		// TODO convert the List<CellInfo> from TelephonyManager to a
-		// List<ICellInfo>
-		return new ArrayList<ICellInfo>();
-	}
+        // Start listening to phone state
+        telephonyManager.listen(phoneStateListener, events);
 
-	@Override
-	public int getCallState() {
-		// We normally should convert the call state code given by
-		// TelephonyManager to the given code in the ITelephony interface. But
-		// as today they are the same.
-		return callState;
-	}
+        super.onCreate();
+    }
 
-	@Override
-	public int getDataState() {
-		// We normally should convert the data state code given by
-		// TelephonyManager to the given code in the ITelephony interface. But
-		// as today they are the same.
-		return dataState;
-	}
+    @Override
+    public void onDestroy() {
+        // Unregister our listener from the telephony manager system service
+        telephonyManager.listen(phoneStateListener,
+                PhoneStateListener.LISTEN_NONE);
+        super.onDestroy();
+    }
 
-	@Override
-	public int getNetworkType() {
-		// We normally should convert the network type code given by
-		// TelephonyManager to the given code in the ITelephony interface. But
-		// as today they are the same.
-		return networkType;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d("TelephonyService", intent.toString());
+        // Return our interface binder
+        return mTelephonyBinder;
+    }
 
-	@Override
-	public ILocation getLocation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ICellInfo> getAllCellInfo() {
+        // TODO convert the List<CellInfo> from TelephonyManager to a
+        // List<ICellInfo>
+        return new ArrayList<ICellInfo>();
+    }
 
-	@Override
-	public ISignalStrength getSignalStrength() {
-		// Return the current signal strength
-		return signalStrength;
-	}
+    @Override
+    public int getCallState() {
+        // We normally should convert the call state code given by
+        // TelephonyManager to the given code in the ITelephony interface. But
+        // they are the same so far.
+        return callState;
+    }
 
-	@Override
-	public void listen(TelephonyListener listener, int events) {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public int getDataState() {
+        // We normally should convert the data state code given by
+        // TelephonyManager to the given code in the ITelephony interface. But
+        // they are the same so far.
+        return dataState;
+    }
 
-	@Override
-	public void setMinimumRefreshRate(int milliseconds) {
-		// TODO Auto-generated method stub
+    @Override
+    public int getNetworkType() {
+        // We normally should convert the network type code given by
+        // TelephonyManager to the given code in the ITelephony interface. But
+        // they are the same so far.
+        return networkType;
+    }
 
-	}
+    @Override
+    public ILocation getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ISignalStrength getSignalStrength() {
+        // Return the current signal strength
+        return signalStrength;
+    }
+
+    @Override
+    public void listen(TelephonyListener listener, int events) {
+        // Check if we are unregistering a listener
+        if (events == TelephonyListener.LISTEN_NONE) {
+            // Unregister the listener from all the lists
+            listenersCallState.remove(listener);
+            listenersCellInfo.remove(listener);
+            listenersDataState.remove(listener);
+            listenersLocation.remove(listener);
+            listenersNetworkType.remove(listener);
+            listenersSignalStrength.remove(listener);
+        } else {
+            // Add the listener to the corresponding list, according to the
+            // events
+            // it is subscribing to.
+            // We are making bitwise comparison because 'events' is used as a
+            // boolean mask.
+            if ((events & TelephonyListener.LISTEN_CALL_STATE) == TelephonyListener.LISTEN_CALL_STATE) {
+                // The listener wish to monitor the call state, add it to the
+                // list
+                listenersCallState.add(listener);
+            } else if ((events & TelephonyListener.LISTEN_CELL_INFO) == TelephonyListener.LISTEN_CELL_INFO) {
+                // The listener wish to monitor the cells infos, add it to the
+                // list
+                listenersCellInfo.add(listener);
+            } else if ((events & TelephonyListener.LISTEN_DATA_CONNECTION_STATE) == TelephonyListener.LISTEN_DATA_CONNECTION_STATE) {
+                // The listener wish to monitor the data state, add it to the
+                // list
+                listenersDataState.add(listener);
+            } else if ((events & TelephonyListener.LISTEN_LOCATION) == TelephonyListener.LISTEN_LOCATION) {
+                // The listener wish to monitor the location, add it to the list
+                listenersLocation.add(listener);
+            } else if ((events & TelephonyListener.LISTEN_NETWORK_TYPE) == TelephonyListener.LISTEN_NETWORK_TYPE) {
+                // The listener wish to monitor the network type, add it to the
+                // list
+                listenersNetworkType.add(listener);
+            } else if ((events & TelephonyListener.LISTEN_SIGNAL_STRENGTHS) == TelephonyListener.LISTEN_SIGNAL_STRENGTHS) {
+                // The listener wish to monitor the signal strength, add it to
+                // the list
+                listenersSignalStrength.add(listener);
+            }
+        }
+    }
+
+    @Override
+    public void setMinimumRefreshRate(int milliseconds) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
