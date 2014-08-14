dedb421742f74e6e50a2915e5ead5f6f447367c0
Nicolas Gaborit <gaborit.nico@gmail.com>
2014-08-14 11:01:12 +0200

    Added RecordingHandler

---
+++ src/com/qualoutdoor/recorder/recording/RecordingHandler.java
@@ -0,0 +1,20 @@
+package com.qualoutdoor.recorder.recording;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class RecordingHandler extends Handler {
+
+    
+    public RecordingHandler() {
+        
+    }
+    
+    @Override
+    public void handleMessage(Message msg) {
+        // TODO Auto-generated method stub
+        super.handleMessage(msg);
+    }
+    
+    
+}
